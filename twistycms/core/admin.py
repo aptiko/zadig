f034f0fb3689fe54b120f9bd74b61b3d44ac7035
Antonis Christofides <anthony@itia.ntua.gr>
2010-03-26 13:02:47 +0200

    Removed early development garbage

--- twistycms/core/admin.py
+++ twistycms/core/admin.py
@@ -3,10 +3,3 @@ import models
 
 admin.site.register(models.Language)
 admin.site.register(models.Site)
-admin.site.register(models.ContentFormat)
-
-class PageInline(admin.StackedInline): model = models.Page
-class MetatagsInline(admin.TabularInline): model = models.VObjectMetatags
-class PageEntryAdmin(admin.ModelAdmin):
-    inlines = [PageInline, MetatagsInline]
-admin.site.register(models.Entry, PageEntryAdmin)
