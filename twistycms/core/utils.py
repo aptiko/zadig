c5186b171e2f2edd3b4d56b1cf06f6ce02882d9f
Antonis Christofides <anthony@itia.ntua.gr>
2010-06-04 15:48:18 +0300

    'align' attribute is now allowed in html

--- twistycms/core/utils.py
+++ twistycms/core/utils.py
@@ -82,7 +82,7 @@ class sanitize_html(unicode):
     # FIXME: Needs unit testing
     valid_tags = '''p i strong em b u a h1 h2 pre br img table thead tbody td
                     tr'''.split()
-    valid_attrs = 'href src width height class'.split()
+    valid_attrs = 'href src width height class align'.split()
     url_attrs = 'href src'.split()
     valid_schemes = 'http https ftp mailto'.split()
     def __new__(cls, html):
