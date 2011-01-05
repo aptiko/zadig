7737c44747e63a3daa60bb6d88eebbe78d78f223
Antonis Christofides <anthony@itia.ntua.gr>
2011-01-05 15:50:24 +0200

    Migrations for statetransition.lentity and lentity.special

---
+++ zadig/core/migrations/0003_statetransition_lentity.py
@@ -0,0 +1,157 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+    
+    def forwards(self, orm):
+        
+        # Changing field 'VObject.deletion_mark'
+        db.alter_column('zadig_vobject', 'deletion_mark', self.gf('django.db.models.fields.BooleanField')(blank=True))
+
+        # Adding field 'StateTransition.lentity'
+        db.add_column('zadig_statetransition', 'lentity', self.gf('django.db.models.fields.related.ForeignKey')(default=300, to=orm['core.Lentity']), keep_default=False)
+
+        # Removing unique constraint on 'StateTransition', fields ['target_state', 'source_state']
+        db.delete_unique('zadig_statetransition', ['target_state_id', 'source_state_id'])
+    
+    
+    def backwards(self, orm):
+        
+        # Changing field 'VObject.deletion_mark'
+        db.alter_column('zadig_vobject', 'deletion_mark', self.gf('django.db.models.fields.BooleanField')())
+
+        # Deleting field 'StateTransition.lentity'
+        db.delete_column('zadig_statetransition', 'lentity_id')
+
+        # Adding unique constraint on 'StateTransition', fields ['target_state', 'source_state']
+        db.create_unique('zadig_statetransition', ['target_state_id', 'source_state_id'])
+    
+    
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'core.entry': {
+            'Meta': {'unique_together': "(('container', 'name'), ('container', 'seq'))", 'object_name': 'Entry', 'db_table': "'zadig_entry'"},
+            'btemplate': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'container': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'all_subentries'", 'null': 'True', 'to': "orm['core.Entry']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'multilingual_group': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.MultilingualGroup']", 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.SlugField', [], {'db_index': 'True', 'max_length': '100', 'blank': 'True'}),
+            'object_class': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'seq': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'state': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.State']"})
+        },
+        'core.entrypermission': {
+            'Meta': {'unique_together': "(('lentity', 'permission'),)", 'object_name': 'EntryPermission', 'db_table': "'zadig_entrypermission'"},
+            'entry': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Entry']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lentity': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Lentity']"}),
+            'permission': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Permission']"})
+        },
+        'core.language': {
+            'Meta': {'object_name': 'Language', 'db_table': "'zadig_language'"},
+            'descr': ('django.db.models.fields.CharField', [], {'max_length': '63'}),
+            'id': ('django.db.models.fields.CharField', [], {'max_length': '5', 'primary_key': 'True'})
+        },
+        'core.lentity': {
+            'Meta': {'unique_together': "(('user', 'group'),)", 'object_name': 'Lentity', 'db_table': "'zadig_lentity'"},
+            'group': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.Group']", 'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'special': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True'})
+        },
+        'core.multilingualgroup': {
+            'Meta': {'object_name': 'MultilingualGroup', 'db_table': "'zadig_multilingualgroup'"},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'core.permission': {
+            'Meta': {'object_name': 'Permission', 'db_table': "'zadig_permission'"},
+            'descr': ('django.db.models.fields.CharField', [], {'max_length': '31'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'core.state': {
+            'Meta': {'object_name': 'State', 'db_table': "'zadig_state'"},
+            'descr': ('django.db.models.fields.CharField', [], {'max_length': '31'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'core.statepermission': {
+            'Meta': {'object_name': 'StatePermission', 'db_table': "'zadig_statepermission'"},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lentity': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Lentity']"}),
+            'permission': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Permission']"}),
+            'state': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.State']"})
+        },
+        'core.statetransition': {
+            'Meta': {'object_name': 'StateTransition', 'db_table': "'zadig_statetransition'"},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lentity': ('django.db.models.fields.related.ForeignKey', [], {'default': '300', 'to': "orm['core.Lentity']"}),
+            'source_state': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'source_rules'", 'to': "orm['core.State']"}),
+            'target_state': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'target_rules'", 'to': "orm['core.State']"})
+        },
+        'core.vobject': {
+            'Meta': {'unique_together': "(('entry', 'version_number'),)", 'object_name': 'VObject', 'db_table': "'zadig_vobject'"},
+            'date': ('django.db.models.fields.DateTimeField', [], {}),
+            'deletion_mark': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'entry': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'vobject_set'", 'to': "orm['core.Entry']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Language']", 'null': 'True', 'blank': 'True'}),
+            'object_class': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'version_number': ('django.db.models.fields.PositiveIntegerField', [], {})
+        },
+        'core.vobjectmetatags': {
+            'Meta': {'unique_together': "(('vobject', 'language'),)", 'object_name': 'VObjectMetatags', 'db_table': "'zadig_vobjectmetatags'"},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Language']"}),
+            'short_title': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'vobject': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'metatags_set'", 'to': "orm['core.VObject']"})
+        },
+        'core.workflow': {
+            'Meta': {'object_name': 'Workflow', 'db_table': "'zadig_workflow'"},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '127'}),
+            'state_transitions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['core.StateTransition']", 'symmetrical': 'False'}),
+            'states': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['core.State']", 'symmetrical': 'False'})
+        }
+    }
+    
+    complete_apps = ['core']
