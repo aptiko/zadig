7abf2b7a8eda82ec9977a26167c080062f6b1c08
Antonis Christofides <anthony@itia.ntua.gr>
2011-06-13 18:28:35 +0300

    More Django version compatibility fixes

--- settings-base.py
+++ settings-base.py
@@ -1,8 +1,8 @@
 USE_I18N = True
 
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -17,7 +17,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'urls'
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.auth",
+    "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.request",
