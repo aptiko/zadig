8682299c25f9257eea6408c7a3697047b58f13c9
Antonis Christofides <anthony@itia.ntua.gr>
2011-12-05 14:27:03 +0200

    Forgotten import

--- zadig/zblog/models.py
+++ zadig/zblog/models.py
@@ -1,7 +1,6 @@
 from django.utils.translation import ugettext as _
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from django.http import Http404
 from django import forms
 from django.core.mail import send_mail
 from django.db import models
